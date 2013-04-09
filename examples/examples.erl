2eb47ed0c2c49c738761a5224019393011bfc2db
Yuriy Iskra <iskra.yw@gmail.com>
2013-04-09 19:29:47 +0300

    Add tests and examples

---
+++ examples/examples.erl
@@ -0,0 +1,13 @@
+-module(examples).
+-compile(export_all).
+
+-record(person, {name, age}).
+-record(person2, {name, age, phone}).
+
+encoder() ->
+    jsonx:encoder([{person,   record_info(fields, person)},
+                   {person2,  record_info(fields, person2)} ]).
+
+decoder() ->
+    jsonx:decoder([{person,   record_info(fields, person)},
+                   {person2,  record_info(fields, person2)} ]).
